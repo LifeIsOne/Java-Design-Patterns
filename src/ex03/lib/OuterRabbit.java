4d608d10ea20bfada4f4544bbce2e778e0ae0c91
LifeIsOne <didtmdgh258@gmail.com>
2024-05-28 09:43:35 +0900

    Adapter 패턴

---
+++ src/ex03/lib/OuterRabbit.java
@@ -0,0 +1,13 @@
+package ex03.lib;
+
+/**
+ *  외부에서 만든 토끼 ( 코드수정 X )
+ *  목적 : OuterRabbit을 문지기에게 전달하기
+ */
+public class OuterRabbit {
+    private String fullname = "토끼";
+
+    public String getfullName() {
+        return fullname;
+    }
+}
