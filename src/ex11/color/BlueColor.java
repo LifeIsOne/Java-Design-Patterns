4da6e4d3c0f4e07bbb51c237ea0c754d755d018f
LifeIsOne <didtmdgh258@gmail.com>
2024-06-03 10:25:49 +0900

    ( 디자인 패턴 ) 브리지 패턴

--- src/ex11/color/BlueColor.java
+++ src/ex11/color/BlueColor.java
@@ -2,7 +2,6 @@ package ex11.color;
 
 public class BlueColor implements Color{
 
-
     @Override
     public void setColor() {
         System.out.println("🟦파란색 적용");
--- src/ex11/color/RedColor.java
+++ src/ex11/color/RedColor.java
@@ -2,7 +2,6 @@ package ex11.color;
 
 public class RedColor implements Color{
 
-
     @Override
     public void setColor() {
         System.out.println("🟥빨간색 적용");
